70ec2d21d6838e558c419087db211ad6389da785
eiyahigaki <eiyahigaki@gmail.com>
2021-12-30 18:46:01 +0900

    state_idにActiveHashを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,12 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :state
 
   belongs_to :user
   has_one_attached :image
 
-  validates :title, :detail, :category_id, presence:true
+  validates :title, :detail, :category_id, :state_id, presence:true
 
-  validates :category_id, numericality: { other_than: 1 , message: "can`t be blank"}
+  validates :category_id, :state_id, numericality: { other_than: 1 , message: "can`t be blank"}
 end
