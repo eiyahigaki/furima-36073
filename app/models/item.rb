257b71710bad444a3039ed19748761ea3d1c1d8b
eiyahigaki <eiyahigaki@gmail.com>
2021-12-30 19:33:49 +0900

    ActiveHashの導入が完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,13 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :state
   belongs_to :charge
+  belongs_to :area
+  belongs_to :term
 
   belongs_to :user
   has_one_attached :image
 
-  validates :title, :detail, :category_id, :state_id, :charge_id, presence:true
+  validates :title, :detail, :category_id, :state_id, :charge_id, :area_id, :term_id, presence:true
 
-  validates :category_id, :state_id, :charge_id, numericality: { other_than: 1 , message: "can`t be blank"}
+  validates :category_id, :state_id, :charge_id, :area_id, :term_id, numericality: { other_than: 1 , message: "can`t be blank"}
 end
