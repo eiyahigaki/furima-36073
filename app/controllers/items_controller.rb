e267baa4c29ef466fbb7a459db026131664dcb9a
eiyahigaki <eiyahigaki@gmail.com>
2021-12-30 18:36:02 +0900

    category_idにActiveHashを導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,10 +3,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
-  def item_params
-    params.require(:item).permit(:image, :title, :detail).merge(user_id: current_user.id)
 
+  def item_params
+    params.require(:item).permit(:image, :title, :detail, :category_id).merge(user_id: current_user.id)
+  end
 end
