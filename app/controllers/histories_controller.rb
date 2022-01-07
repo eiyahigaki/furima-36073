f763a700fc0118b25f2bff8f329c6432b085314c
eiyahigaki <eiyahigaki@gmail.com>
2022-01-07 21:46:06 +0900

    手順4 フォームの情報を保存できるようにする

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -4,4 +4,21 @@ class HistoriesController < ApplicationController
   def index
     @history_delivery = HistoryDelivery.new
   end
+
+  def create
+    binding.pry
+    @history_delivery = HistoryDelivery.new(history_params)
+    if @history_delivery.valid?
+      @history_delivery.save
+      redirect root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def history_params
+    params.require(:history_delivery).permit(:postal_code, :area_id, :city, :address, :building, :tel).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
