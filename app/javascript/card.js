b82219c6387290de6e7399428ee21f190283d442
eiyahigaki <eiyahigaki@gmail.com>
2022-01-10 11:04:13 +0900

    ③トークンが発行されたことを確認する

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,9 +1,26 @@
 const pay = () => {
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log("フォーム送信時にイベント発火")
-  })
+    
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("history_delivery[number]"),
+      cvc: formData.get("history_delivery[cvc]"),
+      exp_month: formData.get("history_delivery[exp_month]"),
+      exp_year: `20${formData.get("history_delivery[exp_year]")}`,
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200){
+        const token = response.id;
+        console.log(token)
+      }
+    })
+  });
 };
 
 window.addEventListener("load", pay);
